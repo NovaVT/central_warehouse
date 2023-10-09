35e70152bb90b626949cc5da0bfa1e4dbf6927e4
NovaVT <1584773938@qq.com>
2023-10-09 19:49:45 +0800

    增加感叹号

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"Hello workSpace2";
-    cout<<"Hello workSpace1 ！！！！";
+    cout<<"Hello workSpace2"<< endl;
+    cout<<"Hello workSpace1 ！！！！！！！！"<< endl;//感叹号太少了
+    cout<<"Hello main,take me in"<< endl;
     return 0;
 }

be30d4d6dc8d2300f8bc9c684d19193cceef6ff0
NovaVT <1584773938@qq.com>
2023-10-09 19:44:08 +0800

    减少感叹号

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     cout<<"Hello workSpace2"<< endl;
-    cout<<"Hello workSpace1 ！！！！"<< endl;
+    cout<<"Hello workSpace1 ！"<< endl; //感叹号太多了
     cout<<"Hello main,take me in"<< endl;
     return 0;
 }
